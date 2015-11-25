d243ef74268a45dfe69763e5d968cc60dc4b8b2b
Nic Aitch <nic@nicinabox.com>
2015-11-25 15:21:00 -0600

    Update to babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: [/node_modules/],
-        loader: 'babel'
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        }
       }
     ]
   },
