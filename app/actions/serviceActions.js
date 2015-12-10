8ec14443293120c6e8e41ea33366444b69d02000
Nic Aitch <nic@nicinabox.com>
2015-12-09 19:07:58 -0600

    ES6-ify serviceActions

--- app/actions/serviceActions.js
+++ app/actions/serviceActions.js
@@ -1,60 +1,58 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher');
-var appConstants = require('../constants/appConstants');
-var dropbox = require('../utils/dropbox');
+import AppDispatcher from '../dispatcher/AppDispatcher'
+import appConstants from '../constants/appConstants'
+import dropbox from '../utils/dropbox'
 
-var serviceActions = {
-  setActiveService: function(service) {
+export default {
+  setActiveService(service) {
     AppDispatcher.handleAction({
       actionType: appConstants.SET_ACTIVE_SERVICE,
       data: service
-    });
+    })
   },
 
-  clearActiveService: function() {
+  clearActiveService() {
     AppDispatcher.handleAction({
       actionType: appConstants.CLEAR_ACTIVE_SERVICE
-    });
+    })
   },
 
-  focusResult: function () {
+  focusResult() {
     AppDispatcher.handleAction({
       actionType: appConstants.FOCUS_RESULT
-    });
+    })
   },
 
-  blurResult: function () {
+  blurResult() {
     AppDispatcher.handleAction({
       actionType: appConstants.BLUR_RESULT
-    });
+    })
   },
 
-  filterServices: function(name) {
+  filterServices(name) {
     AppDispatcher.handleAction({
       actionType: appConstants.FILTER_SERVICES,
       data: name
-    });
+    })
   },
 
-  matchSavedService: function (name) {
+  matchSavedService(name) {
     AppDispatcher.handleAction({
       actionType: appConstants.MATCH_SAVED_SERVICE,
       data: name
-    });
+    })
   },
 
-  saveService: function(service) {
+  saveService(service) {
     AppDispatcher.handleAction({
       actionType: appConstants.SAVE_SERVICE,
       data: service
-    });
+    })
   },
 
-  removeService: function(service) {
+  removeService(service) {
     AppDispatcher.handleAction({
       actionType: appConstants.REMOVE_SERVICE,
       data: service
-    });
+    })
   }
-};
-
-module.exports = serviceActions;
+}
